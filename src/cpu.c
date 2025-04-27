bd80c049384bffa0a2f5af8cd4d72572532db19b
TheVinh203 <vinh.nongthevinh203@hcmut.edu.vn>
2025-04-27 09:43:32 +0700

    Update cpu.c

--- src/cpu.c
+++ src/cpu.c
@@ -1,112 +1,99 @@
-
+/* ---------------  Simple “CPU” execution loop  ------------------- */
 #include "cpu.h"
 #include "mem.h"
 #include "mm.h"
 #include "syscall.h"
 #include "libmem.h"
+#include <stdio.h>
 
-int calc(struct pcb_t *proc)
-{
-	return ((unsigned long)proc & 0UL);
-}
+#define MAX_GPR  NUM_REGS
+static inline int gpr_ok(unsigned r) { return r < MAX_GPR; }
 
-int alloc(struct pcb_t *proc, uint32_t size, uint32_t reg_index)
-{
-	addr_t addr = alloc_mem(size, proc);
-	if (addr == 0)
-	{
-		return 1;
-	}
-	else
-	{
-		proc->regs[reg_index] = addr;
-		return 0;
-	}
-}
+/* ---------- helpers when *no* paging is compiled in -------------- */
+static int  calc       (struct pcb_t *p)                       { return ((unsigned)p & 0UL); }
+static int  alloc_plain(struct pcb_t *p,uint32_t sz,uint32_t r){ addr_t a=alloc_mem(sz,p);if(!a)return 1;p->regs[r]=a;return 0;}
+static int  free_plain (struct pcb_t *p,uint32_t r)            { return free_mem(p->regs[r],p); }
+static int  read_plain (struct pcb_t *p,uint32_t s,uint32_t o,uint32_t d){BYTE v;if(!read_mem(p->regs[s]+o,p,&v))return 1;p->regs[d]=v;return 0;}
+static int  write_plain(struct pcb_t *p,BYTE v,uint32_t d,uint32_t o)   { return write_mem(p->regs[d]+o,p,v); }
 
-int free_data(struct pcb_t *proc, uint32_t reg_index)
-{
-	return free_mem(proc->regs[reg_index], proc);
-}
-
-int read(
-	struct pcb_t *proc, // Process executing the instruction
-	uint32_t source,	// Index of source register
-	uint32_t offset,	// Source address = [source] + [offset]
-	uint32_t destination)
-{ // Index of destination register
-
-	BYTE data;
-	if (read_mem(proc->regs[source] + offset, proc, &data))
-	{
-		proc->regs[destination] = data;
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
-}
-
-int write(
-	struct pcb_t *proc,	// Process executing the instruction
-	BYTE data,		// Data to be wrttien into memory
-	uint32_t destination, // Index of destination register
-	uint32_t offset)
-{ // Destination address =
-	// [destination] + [offset]
-	return write_mem(proc->regs[destination] + offset, proc, data);
-}
-
+/* ================================================================= */
 int run(struct pcb_t *proc)
 {
-	/* Check if Program Counter point to the proper instruction */
-	if (proc->pc >= proc->code->size)
-	{
-		return 1;
-	}
+    if (proc->pc >= proc->code->size) return 1;          /* finished */
 
-	struct inst_t ins = proc->code->text[proc->pc];
-	proc->pc++;
-	int stat = 1;
-switch (ins.opcode)
-	{
-	case CALC:
-		stat = calc(proc);
-		break;
-	case ALLOC:
+    struct inst_t ins = proc->code->text[proc->pc++];
+
+    /* DBG-BEGIN : one-liner that shows *every* instruction executed  */
+    printf("[CPU] pid=%d pc=%04u  opcode=%d  mm=%p\n",
+           proc->pid, proc->pc-1, ins.opcode, (void*)proc->mm);
+    /* DBG-END   ---------------------------------------------------- */
+
+    int rc = 1;                                         /* default = stop */
+
+    switch (ins.opcode)
+    {
+/* ---- CALC ------------------------------------------------------- */
+    case CALC:   rc = calc(proc);                        break;
+
+/* ---- ALLOC ------------------------------------------------------ */
+    case ALLOC:
 #ifdef MM_PAGING
-		stat = liballoc(proc, ins.arg_0, ins.arg_1);
+        rc = liballoc(proc, ins.arg_0, ins.arg_1);
 #else
-		stat = alloc(proc, ins.arg_0, ins.arg_1);
+        if (gpr_ok(ins.arg_1)) rc = alloc_plain(proc, ins.arg_0, ins.arg_1);
 #endif
-		break;
-	case FREE:
+        break;
+
+/* ---- FREE ------------------------------------------------------- */
+    case FREE:
 #ifdef MM_PAGING
-		stat = libfree(proc, ins.arg_0);
+        rc = libfree(proc, ins.arg_0);
 #else
-		stat = free_data(proc, ins.arg_0);
+        if (gpr_ok(ins.arg_0)) rc = free_plain(proc, ins.arg_0);
 #endif
-		break;
-	case READ:
+        break;
+
+/* ---- READ ------------------------------------------------------- */
+    case READ:
 #ifdef MM_PAGING
-		stat = libread(proc, ins.arg_0, ins.arg_1, &ins.arg_2);
+        if (!gpr_ok(ins.arg_2)) { puts("[CPU] READ bad dst"); break; }
+
+        printf("[CPU] READ  r%u + %u -> r%u ?\n",
+               ins.arg_0, ins.arg_1, ins.arg_2);
+
+        rc = libread(proc,
+                     ins.arg_0,                /* region id   */
+                     ins.arg_1,                /* offset      */
+                     &proc->regs[ins.arg_2]);  /* dest reg    */
+
+        if (rc==0)
+            printf("[CPU]   → %u (stored in r%u)\n",
+                   proc->regs[ins.arg_2], ins.arg_2);
 #else
-		stat = read(proc, ins.arg_0, ins.arg_1, ins.arg_2);
+        if (gpr_ok(ins.arg_0)&&gpr_ok(ins.arg_2))
+            rc = read_plain(proc, ins.arg_0, ins.arg_1, ins.arg_2);
 #endif
-		break;
-	case WRITE:
+        break;
+
+/* ---- WRITE ------------------------------------------------------ */
+    case WRITE:
 #ifdef MM_PAGING
-		stat = libwrite(proc, ins.arg_0, ins.arg_1, ins.arg_2);
+        rc = libwrite(proc,(BYTE)ins.arg_0,ins.arg_1,ins.arg_2);
 #else
-		stat = write(proc, ins.arg_0, ins.arg_1, ins.arg_2);
+        if (gpr_ok(ins.arg_1))
+            rc = write_plain(proc, ins.arg_0, ins.arg_1, ins.arg_2);
 #endif
-		break;
-	case SYSCALL:
-		stat = libsyscall(proc, ins.arg_0, ins.arg_1, ins.arg_2, ins.arg_3);
-		break;
-	default:
-		stat = 1;
-	}
-	return stat;
+        break;
+
+/* ---- SYSCALL ---------------------------------------------------- */
+    case SYSCALL:
+        rc = libsyscall(proc, ins.arg_0, ins.arg_1, ins.arg_2, ins.arg_3);
+        break;
+
+/* ---- unknown ---------------------------------------------------- */
+    default:
+        printf("[CPU] unknown opcode %d – halted\n", ins.opcode);
+    }
+
+    return rc;                                         /* 0 keep running */
 }
